aa877ceea6279efeb59ae663c0591daa17b2cba3
heywon0909 <hw731911@gmail.com>
2024-12-02 18:32:16 +0900

    feat: app component 디자인 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Bunney } from 'bunney-ui/Bunney'
+import styles from './App.module.css'
 import './App.css'
 
+import { Logo } from 'bunney-ui/Logo'
+import { BunneyLoading } from 'bunney-ui/BunneyLoading'
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className={styles.flex_col}>
+        <div className={styles.flex}>
+          <h1 className={styles.title}>SCREEN TRACKER</h1>
+          <Logo size={3} />
+        </div>
+        <div>  
+          <h3 className={styles.detail}>with JumpingBunnney</h3>
+        </div>
+        </div> 
+          <div className={styles.content}>
+            <BunneyLoading/>
+        </div>
+        <button>choose option</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       
     </>
   )
 }
